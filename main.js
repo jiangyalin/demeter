4f3764871f704c63bb21f08e4dc5bbd5456ee266
yalin.jiang@asapone.cn <995991>
2022-10-21 17:49:27 +0800

    feat(terminal): 新增terminal服务

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const pkg = require('./package')
 const routes = require('./routes/')
 const config = require('./config')
 const { createServer } = require('http')
-const { Server } = require('socket.io')
+// const { Server } = require('socket.io')
 const bodyParser = require('body-parser')
+const expressWs = require('express-ws')
+const pty = require('node-pty')
+// const userhome = require('user-home')
+const server = createServer(app)
+// const io = new Server(server, { cors: true })
 // const formidable = require('express-formidable')
 // const http = require('http')
 const os = require('os')
@@ -30,6 +35,8 @@ const os = require('os')
 
 // 路由重定向不能与接口同时使用
 // app.use(history());
+
+expressWs(app)
 
 // 跨域
 app.all('*', function (req, res, next) {
@@ -70,13 +77,84 @@ const getIpAddress = () => {
 
 routes(app)
 
-// const app = require('express')()
-const server = createServer(app)
-const io = new Server(server, { cors: true })
-io.on('connection', () => {
-  console.log('mmm')
+const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash'
+const termMap = new Map() // 存储 pty 实例，通过 pid 映射
+function nodeEnvBind () {
+  //绑定当前系统 node 环境
+  const term = pty.spawn(shell, ['--login'], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 24,
+    cwd: process.env.HOME,
+    env: process.env
+  })
+  termMap.set(term.pid, term)
+  return term
+}
+
+//服务端初始化
+app.post('/terminal', (req, res) => {
+  const term = nodeEnvBind(req)
+  res.send({
+    code: 200,
+    data: term.pid.toString(),
+    msg: ''
+  })
+  res.end()
 })
-server.listen(3000)
+
+app.ws('/socket/:pid', (ws, req) => {
+  const pid = parseInt(req.params.pid)
+  const term = termMap.get(pid)
+  term.on('data', function (data) {
+    ws.send(data)
+  })
+  ws.on('message', (data) => {
+    term.write(data)
+  })
+  ws.on('close', function () {
+    term.kill()
+    termMap.delete(pid)
+  })
+})
+
+// io.use('terminal').on('connection', socket => {
+//   console.log('ddd')
+//   let ptyProcess = pty.spawn(shell, ['--login'], {
+//     name: 'xterm-color',
+//     cols: 80,
+//     rows: 24,
+//     cwd: userhome, // 首次进入系统根目录
+//     env: process.env
+//   })
+//
+//   // 绑定data事件，通过socket的output事件，把shell日志传到前端
+//   ptyProcess.on('data', data => socket.emit('output', data))
+//
+//   // socket绑定input事件，用于接收前端发送的命令，并传到ptyProcess中
+//   socket.on('input', data => {
+//     console.log('111')
+//     return ptyProcess.write(data)
+//   })
+//
+//   socket.on('resize', size => {
+//     console.log('222')
+//     ptyProcess.resize(size[0], size[1])
+//   })
+//
+//   socket.on('exit', size => {
+//     console.log('333')
+//     ptyProcess.destroy()
+//   })
+//
+//   console.log('ptyProcess.pid', ptyProcess.pid)
+//   socket.emit('pid', ptyProcess.pid)
+// })
+
+// io.on('connection', () => {
+//   console.log('ws已联通')
+// })
+// server.listen(3000)
 
 app.listen(config.port, function () {
   console.log('服务启动' + config.port)
--- routes/api/file-tree/index.js
+++ routes/api/file-tree/index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const path = require('path')
 const readFileList = require('./../../../tool/read-file-list')
 const router = express.Router()
+const fs = require('fs')
 
+// 获取完整文件树结构
 router.get('/all', (req, res) => {
   const filePath = path.join(__dirname, './../../../data/test')
   const list = readFileList(filePath)
   const treeMap = {}
   let id = 1
 
-  const arrToTree = (arr, tree) => {
+  const arrToTree = (arr, tree, path) => {
     if (!arr.length) {
       return tree
     } else {
@@ -18,17 +20,19 @@ router.get('/all', (req, res) => {
           id: id++,
           name: arr[0],
           type: arr[0].includes('.') ? 'file' : 'folder',
+          path: path,
           node: []
         })
       }
 
-      return arrToTree(arr.splice(1), tree.find(item => item.name === arr[0]).node)
+      return arrToTree(arr.splice(1), tree.find(item => item.name === arr[0]).node, path)
     }
   }
 
   const tree = []
   list.forEach(item => {
-    const pathArr = item.fullPath.substring(filePath.length + 1).split('/')
+    const _path = item.fullPath.substring(filePath.length + 1)
+    const pathArr = _path.split('/')
     pathArr.forEach(node => {
       if (treeMap[node]) {
 
@@ -36,12 +40,28 @@ router.get('/all', (req, res) => {
         treeMap[node] = {}
       }
     })
-    if (!pathArr.includes('.DS_Store')) arrToTree(pathArr, tree)
+    if (!pathArr.includes('.DS_Store')) arrToTree(pathArr, tree, _path)
   })
 
   const data = {
     code: 200,
     data: tree,
+    message: 'ok'
+  }
+  res.jsonp(data)
+})
+
+// 获取文件内容
+router.get('/info', (req, res) => {
+  const _path = req.query.path
+  const prefixPath = '../../../data/test/'
+
+  const fileData = fs.readFileSync(path.join(__dirname, prefixPath + _path), 'utf-8')
+  const data = {
+    code: 200,
+    data: {
+      fileData
+    },
     message: 'ok'
   }
   res.jsonp(data)
